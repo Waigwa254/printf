69f01498b8389d4a6af7cbf5fdfd733f44f87b59
Ndege <kamothowilliam2@gmail.com>
2023-10-16 10:56:34 -0700

    second update

--- int_functions1.c
+++ int_functions1.c
@@ -15,8 +15,8 @@ int print_unsigned(va_list types, char buffer[], int flags,
 {
 	int i = BUFF_SIZE - 2;
 
-	unsigned long int num = va_arg(types, unsigned long int
-	);
+	unsigned long int num = va_arg(types, unsigned long int);
+
 	num = convert_size_unsgnd(num, size);
 
 	if (num == 0)
